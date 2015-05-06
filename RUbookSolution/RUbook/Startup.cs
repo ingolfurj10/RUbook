e45ea03ae98fff8f3ab16416ac0d40c064ecb5b8
Ingo <ingolfurj10@ru.is>
2015-05-06 14:09:05 +0000

    Created starter Visual Studio project and established connection with database

---
+++ RUbookSolution/RUbook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RUbook.Startup))]
+namespace RUbook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
