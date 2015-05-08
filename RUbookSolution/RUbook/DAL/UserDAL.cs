559cc7e257adfb0d6408107a500da60fab60cdcb
Asa <asa13@ru.is>
2015-05-08 21:07:03 +0000

    TimelineViewModel groups, events og posts for Timeline

--- RUbookSolution/RUbook/DAL/UserDAL.cs
+++ RUbookSolution/RUbook/DAL/UserDAL.cs
@@ -1,17 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Mvc;
+using RUbook.DAL;
+using RUbook.Models;
 using Microsoft.AspNet.Identity;
+
 
 namespace RUbook.DAL
 {
     public class UserDAL
     {
 
-       
-        //getUseByID = from u in db.Users where u.Id == id select u).SingleOrDefault()
+        public ApplicationDbContext db = new ApplicationDbContext();
 
-       //Hérna viljum við hafa allar linq queries. 
+        public List<Post> GetAllPosts()
+        {
+
+            var posts = db.Posts.ToList();
+
+            return posts;
+       
+        }
+        public List<Group> GetAllGroups()
+        {
+            var groups = db.Groups.ToList();
+
+            return groups;
+
+        }
+
+        public List<Event> GetAllEvents()
+        {
+            var events = db.Events.ToList();
+
+            return events;
+
+        }
     }
+
 }
+
+//Hérna viljum við hafa allar linq queries. 
