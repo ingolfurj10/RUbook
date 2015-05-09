4ea58592e71a6ca8c44fc43b7e0cea7f8f7a9427
Anna Katrín <annaksnorra@gmail.com>
2015-05-09 00:15:18 +0000

    Post box á tímalínu

--- RUbookSolution/RUbook/Models/TimelineViewModel.cs
+++ RUbookSolution/RUbook/Models/TimelineViewModel.cs
@@ -12,6 +12,7 @@ namespace RUbook.Models
        public List<Post> AllPosts { get; set; }
         public List<Group> AllGroups { get; set; }
         public List<Event> AllEvents { get; set; }
+		public List<Post> Text { get; set; }
         //public Post Post { get; set; } - til að ná í text area for post
     }
 }
