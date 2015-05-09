5679c80e066bc73277c544e0e7cb3d8b50a9ad51
Anna Katrín <annaksnorra@gmail.com>
2015-05-09 21:17:06 +0000

    Bætti við Persónuupplýsingum á notenda

--- RUbookSolution/RUbook/Models/UserInfo.cs
+++ RUbookSolution/RUbook/Models/UserInfo.cs
@@ -13,6 +13,12 @@ namespace RUbook.Models
         public virtual ApplicationUser UserID { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+		public string Age { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public string Education { get; set; }
+		public string WorkInfo { get; set; }
+		public string Department { get; set; }
 		
 
         public virtual ICollection<WorkInfo> WorksInfo { get; set; }
