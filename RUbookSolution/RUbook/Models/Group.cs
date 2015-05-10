5e8209297c1c0051e1616ab7fd12c85ea68fba41
Asa <asa13@ru.is>
2015-05-10 12:36:19 +0000

    Image pælingar

--- RUbookSolution/RUbook/Models/Group.cs
+++ RUbookSolution/RUbook/Models/Group.cs
@@ -12,6 +12,7 @@ namespace RUbook.Models
         public string name { get; set; }
         [Required]
         public string text { get; set; }
+        public string image { get; set; }
         public Department departmentID { get; set; }
         public string course { get; set; }
         //public ApplicationUser userID { get; set; }
