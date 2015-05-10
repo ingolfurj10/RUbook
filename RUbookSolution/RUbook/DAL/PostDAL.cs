701a419fffbd39a3d4e3eae57f3c4cc735b459ea
Asa <asa13@ru.is>
2015-05-10 22:02:12 +0000

    Laga útlit, bæta við postDAL og fyrirspurnum

---
+++ RUbookSolution/RUbook/DAL/PostDAL.cs
@@ -0,0 +1,28 @@
+﻿using RUbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RUbook.DAL
+{
+    public class PostDAL
+    {
+        private ApplicationDbContext db;
+
+        public PostDAL(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public List<Post> GetAllPosts(ApplicationUser User)
+        {
+
+			//var posts = db.Posts.OrderByDescending(p => p.DateCreated).ToList();
+            var posts = db.Posts.Where(p => p.UserID.Id == User.Id).OrderByDescending(p => p.DateCreated).ToList();
+
+            return posts;
+       
+        }
+    }
+}
