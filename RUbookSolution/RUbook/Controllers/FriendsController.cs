4469ef03dd9e690480cbf9e9dbedb42e07f45b68
Asa <asa13@ru.is>
2015-05-11 01:51:39 +0000

    Friends connections

---
+++ RUbookSolution/RUbook/Controllers/FriendsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RUbook.DAL;
+using RUbook.Models;
+using Microsoft.AspNet.Identity;
+
+namespace RUbook.Controllers
+{
+    public class FriendsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        UserDAL userDAL;
+
+        public FriendsController():base()
+        {
+            userDAL = new UserDAL(db);
+        }
+
+        // GET: Friends
+        public ActionResult Index()
+        {
+            return View(db.Friends.ToList());
+        }
+
+        // GET: Friends/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // GET: Friends/Create
+        public ActionResult Create(string id)
+        {
+            Friend ship = new Friend();
+            ship.FriendUserID = userDAL.GetUser(id);
+            ship.UserId = userDAL.GetUser(User.Identity.GetUserId());
+            return View(ship);
+        }
+
+        // POST: Friends/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UserId,FriendUserId")] Friend friend)
+        {
+            var uid = User.Identity.GetUserId();
+
+            //if (id == uid)
+            //{
+            //     ekki hægt að vera vinur sinn
+            //}
+
+            var user = userDAL.GetUser(uid);
+            var fr = userDAL.GetUser(friend.FriendUserID.Id);
+
+            Friend ship = new Friend();
+            ship.UserId = user;
+            ship.FriendUserID = fr;
+
+            db.Friends.Add(ship);
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: Friends/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // POST: Friends/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID")] Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(friend).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(friend);
+        }
+
+        // GET: Friends/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // POST: Friends/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Friend friend = db.Friends.Find(id);
+            db.Friends.Remove(friend);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
