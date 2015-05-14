59eae3f90804001185ad7c532bac5f5426352a9b
Asa <asa13@ru.is>
2015-05-14 22:51:39 +0000

    tiltekt

--- RUbookSolution/RUbook/Controllers/GroupMembersController.cs
+++ RUbookSolution/RUbook/Controllers/GroupMembersController.cs
@@ -26,29 +26,28 @@ namespace RUbook.Controllers
             userDAL = new UserDAL(db);
         }
 
-        // GET: GroupMembers
-        public ActionResult Index()
-        {
-            return View(db.GroupMembers.ToList());
-        }
+        //// GET: GroupMembers
+        //public ActionResult Index()
+        //{
+        //    return View(db.GroupMembers.ToList());
+        //}
 
-        // GET: GroupMembers/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GroupMember groupMember = db.GroupMembers.Find(id);
-            if (groupMember == null)
-            {
-                return HttpNotFound();
-            }
-            return View(groupMember);
-        }
+        //// GET: GroupMembers/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    GroupMember groupMember = db.GroupMembers.Find(id);
+        //    if (groupMember == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(groupMember);
+        //}
 
         // GET: GroupMembers/Create
-     
         public ActionResult Create(int id)
         {
             GroupMemberViewModel g = new GroupMemberViewModel();
@@ -61,17 +60,12 @@ namespace RUbook.Controllers
         }
 
         // POST: GroupMembers/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "GroupId")] GroupMemberViewModel model)
         {
             var uid = User.Identity.GetUserId();
             var user = userDAL.GetUser(uid);
-
-            
-
 
             GroupMember gm = new GroupMember();
             gm.GroupID = model.GroupId;
@@ -83,62 +77,62 @@ namespace RUbook.Controllers
             return RedirectToAction("Details", "Groups", new { id = gm.GroupID });
         }
 
-        // GET: GroupMembers/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GroupMember groupMember = db.GroupMembers.Find(id);
-            if (groupMember == null)
-            {
-                return HttpNotFound();
-            }
-            return View(groupMember);
-        }
+        //// GET: GroupMembers/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    GroupMember groupMember = db.GroupMembers.Find(id);
+        //    if (groupMember == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(groupMember);
+        //}
 
-        // POST: GroupMembers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID")] GroupMember groupMember)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(groupMember).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(groupMember);
-        }
+        //// POST: GroupMembers/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "ID")] GroupMember groupMember)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(groupMember).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(groupMember);
+        //}
 
-        // GET: GroupMembers/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GroupMember groupMember = db.GroupMembers.Find(id);
-            if (groupMember == null)
-            {
-                return HttpNotFound();
-            }
-            return View(groupMember);
-        }
+        //// GET: GroupMembers/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    GroupMember groupMember = db.GroupMembers.Find(id);
+        //    if (groupMember == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(groupMember);
+        //}
 
-        // POST: GroupMembers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            GroupMember groupMember = db.GroupMembers.Find(id);
-            db.GroupMembers.Remove(groupMember);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: GroupMembers/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    GroupMember groupMember = db.GroupMembers.Find(id);
+        //    db.GroupMembers.Remove(groupMember);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
         protected override void Dispose(bool disposing)
         {
