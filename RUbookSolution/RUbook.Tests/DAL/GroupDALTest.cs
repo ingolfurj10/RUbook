a8a5781b5be0e412153cd6b7053d5544463a0a25
Berglind <beggath81@gmail.com>
2015-05-14 16:46:26 +0000

    unit test

--- RUbookSolution/RUbook.Tests/DAL/GroupDALTest.cs
+++ RUbookSolution/RUbook.Tests/DAL/GroupDALTest.cs
@@ -14,23 +14,11 @@ namespace RUbook.Tests.DAL
         {
             ApplicationDbContext db = new ApplicationDbContext();
             //Arrange:
-           // const string user = "sombody"; //ákveðið ID = ákveðinn  notandi
+            // const string user = "sombody"; //ákveðið ID = ákveðinn  notandi
             var DAL = new GroupDAL(db);
             //Act:
             var result = DAL.GetAllGroupsOfUser("4f186767-6ee6-4c54-9ede-846529b7eaf7"); //köllum á fallið 
-
-		public void GetAllGroupsOfUser() //Allar grúppur sem ákv notandi tilheyrir -BÞ
-		{/*
-			//Arrange:
-			const string user = "sombody"; //ákveðið ID = ákveðinn  notandi
-			var DAL = new GroupDAL();
-			//Act:
-			var result = DAL.GetAllGroupsOfUser(user); //köllum á fallið 
-
-			//Assert:
-			Assert.AreEqual(2, result.Count); //þessi notandi tilheyrir 2 hópum*/
-		}
-
+        }
         [TestMethod]
         public void GetGroupMembers() //Allir notendur sem tilheyra ákv grúppu -BÞ 
         {
@@ -45,17 +33,5 @@ namespace RUbook.Tests.DAL
             Assert.AreEqual(4, result.Count); //það tilheyra 4 notendur þessum hópi
         }
     }
-				[TestMethod]
-				public void GetGroupMembers() //Allir notendur sem tilheyra ákv grúppu -BÞ 
-				{
-					//Arrange:
-					/*const int group = "some group id"; 
-					var DAL = new GroupDAL();
-					//Act:
-					var result = DAL.GetGroupMembers(group); //köllum á fallið 
-
-					//Assert:
-					Assert.AreEqual(2, result.Count); //það tilheyra 2 notendr þessum hópi*/
-				}
-	}
+	
 }
