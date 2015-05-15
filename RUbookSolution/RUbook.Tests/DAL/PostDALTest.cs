2c4cae67223de73a293eaf691b4711a9f40fef37
Berglind <beggath81@gmail.com>
2015-05-15 12:30:47 +0000

    unit test - posts

---
+++ RUbookSolution/RUbook.Tests/DAL/PostDALTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RUbook.DAL;
+using Microsoft.AspNet;
+using System.Collections.Generic;
+namespace RUbook.Tests.DAL
+{
+    [TestClass]
+    public class PostDALTest
+    {
+        [TestMethod]
+        public void GetEventPosts()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            //Arrange:
+            var DAL = new PostDAL(db);
+            //Act:
+            var restult = DAL.GetEventPosts(2);
+            //Assert:
+            Assert.AreEqual(4, restult.Count);
+        }
+    }
+}
